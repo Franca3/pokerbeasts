b6a8907eb6585d98422de42780948541e45bc706
Lukas Mulder <lukasmulder@gmail.com>
2018-05-29 15:41:11 +0200

    neural network can now predict radnom outputs

--- neuralnetwork/neuralnetwork.py
+++ neuralnetwork/neuralnetwork.py
@@ -2,10 +2,22 @@ import numpy as np
 
 import weightsInit
 
+import feedForward
+
 class ArtificialNeuralNetwork:
     """ Neural network object"""
     def __init__(self, dimensions, activationFunction):
+        """
+        dimensions is a list of numbers (eg. [7,3,3,2])
+        activationfunctio is a string, either 'sigmoid' or 'linRect
+        """
         self.weights = weightsInit.createWeights(dimensions)
         self.biases = weightsInit.createBiases(dimensions)
+        self.activationFunction = activationFunction
 
-network = ArtificialNeuralNetwork([7,11,11,3],None)
+    def generateOuput(self, inputLayer):
+
+        return feedForward.feedForward(inputLayer, self.weights, self.biases, self.activationFunction)
+
+network = ArtificialNeuralNetwork([7,11,11,3],"sigmoid")
+print(network.generateOuput([0,1,0,1,0,1,0]))
