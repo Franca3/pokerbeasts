d63c0e9168c73496c548ef35d2da9e1161468f5a
Lukas Mulder <lukasmulder@gmail.com>
2018-06-04 21:52:22 +0200

    adjustments for testing

--- game.py
+++ game.py
@@ -7,10 +7,9 @@ from neuralnetwork import feedForward
 if (__name__ == "__main__" ):
     config = setup_config(max_round=10, initial_stack=100, small_blind_amount=5)
     config.register_player(name="p1", algorithm=FishPlayer() )
-    config.register_player(name="p2", algorithm=FishPlayer() )
+    #config.register_player(name="p2", algorithm=FishPlayer() )
     #config.register_player(name="hm", algorithm=HumanPlayer() )
-    #config.register_player(name="p3", algorithm=FishPlayer())
-    config.register_player(name='neuralnetwork', algorithm = NNPlayer([7,11,3],feedForward.sigmoid) )
+    config.register_player(name='neuralnetwork', algorithm = NNPlayer([7,11,3], feedForward.sigmoid) )
     game_result = start_poker(config, verbose = 1)
 
     print(game_result)
